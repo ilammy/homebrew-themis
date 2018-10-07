91d78a99ae3bbf01d0e5368481dd026932d4c70f
ilammy <a.lozovsky@gmail.com>
2018-10-07 19:42:12 +0300

    Add initial formulas for Themis 0.10.0
    Themis is actually a wrapper over low-level cryptographic libraries.
    Currently it supports OpenSSL, LibreSSL, and BoringSSL as backends.
    Homebrew offers only OpenSSL and LibreSSL so that's what we do.
    
    The backend choice is mutually exclusive as the library is named just
    "themis". The user needs to select a particular flavor.  We could have
    provided a single formula with options (--with-{openssl,libressl}), but
    such formulas are frowned upon in the core Homebrew repo, and I don't
    want to check for mutual exclusion. So instead we're going with mutually
    conflicting formulas. This allows the user to install both flavors
    simultaneously, with the drawback of not having Themis in PATH anymore.

---
+++ Formula/themis-libressl.rb
@@ -0,0 +1,32 @@
+class ThemisLibressl < Formula
+  desc "High-level cryptographic primitives (OpenSSL flavor)"
+  homepage "https://www.cossacklabs.com/themis"
+  url "https://github.com/cossacklabs/themis/archive/0.10.0.tar.gz"
+  sha256 "329a981e7e57a90107b330172b738104f7189cb20eac043d708c23c5db1570fb"
+
+  depends_on "libressl"
+  conflicts_with "themis-openssl", :because => "only one flavor of Themis can exist in PATH"
+
+  def install
+    ENV["ENGINE"] = "libressl"
+    ENV["ENGINE_INCLUDE_PATH"] = Formula["libressl"].include
+    ENV["ENGINE_LIB_PATH"] = Formula["libressl"].lib
+    ENV["PREFIX"] = prefix
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOF
+      #include <themis/themis.h>
+      int main(void)
+      {
+        printf("%s", themis_version());
+        return 0;
+      }
+    EOF
+    system ENV.cc, "test.c", "-o", "check-themis-version", "-lthemis"
+    version = shell_output("./check-themis-version")
+    # The 0.10 release is slightly buggy and identifies itself as 0.9
+    assert_match(/\bthemis 0.9\b/, version)
+  end
+end
